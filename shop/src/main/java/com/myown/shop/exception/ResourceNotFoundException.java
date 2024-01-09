ecafec0b00a18109c422469c08e5cb1a0b4f67ef
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-01-09 20:28:25 +0200

    added ProductRepository and exception ResourceNotFoundException

---
+++ shop/src/main/java/com/myown/shop/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.myown.shop.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
