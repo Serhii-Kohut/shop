c7bdfbf5960e7ca219da37c073b9737847320918
Serhii Kohut <113467605+ronycv@users.noreply.github.com>
2024-01-11 17:12:41 +0200

    added RestControllerAdvice - ApplicationErrorHandler
    changed ResourceNotFoundException

---
+++ shop/src/main/java/com/myown/shop/exception/ApplicationErrorHandler.java
@@ -0,0 +1,14 @@
+package com.myown.shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationErrorHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
